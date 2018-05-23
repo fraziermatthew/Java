b5e9b59603e90fec6d3d27ac7f4e500851eb7e4e
fraziermatthew <fraziermatthew@hotmail.com>
2018-05-23 18:12:27 -0400

    Fourth Commit

--- PracticeProblems/Liang_IntroToJavaProgramming_10thEd/Ch_05/Ex_05_03.java
+++ PracticeProblems/Liang_IntroToJavaProgramming_10thEd/Ch_05/Ex_05_03.java
@@ -1,13 +1,13 @@
-/* Conversion from miles to kilometers */
+/* Conversion from kilograms to pounds */
 
 public class Ex_05_03 {
 
    public static void main(String[] args) {
       
-      int num = 10;
-      System.out.printf("%-10s%-10s\n", "Miles","Kilometers");
-      for (double i = 1; i <= num; i++) 
-         System.out.printf("%-10d%-10.3f\n", (int)i, i*1.609);
+      int num = 199;
+      System.out.printf("%-10s%-10s\n", "Kilograms","Pounds");
+      for (double i = 1; i < num; i++) 
+         System.out.printf("%-10d%-10.1f\n", (int)i, i*2.2);
 
    }
 }
---
+++ PracticeProblems/Liang_IntroToJavaProgramming_10thEd/Ch_05/Ex_05_04.java
@@ -0,0 +1,13 @@
+/* Conversion from miles to kilometers */
+
+public class Ex_05_04 {
+
+   public static void main(String[] args) {
+      
+      int num = 10;
+      System.out.printf("%-10s%-10s\n", "Miles","Kilometers");
+      for (double i = 1; i <= num; i++) 
+         System.out.printf("%-10d%-10.3f\n", (int)i, i*1.609);
+
+   }
+}
